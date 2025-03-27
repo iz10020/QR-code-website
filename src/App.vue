1e1ac08c06f3a7a42eb86ab9afdd7a0b8df2a630
iz10020 <iliaszouhairi2023@gmail.com>
2025-03-27 13:05:38 +0100

    update
    homepagina update

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,11 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
   margin-top: 60px;
 }
 
