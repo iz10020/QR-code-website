89108a27498d6189f3cfb0137a7997dd7a489da7
iz10020 <iliaszouhairi2023@gmail.com>
2025-04-09 16:10:16 +0200

    update project
    j

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
-  <!-- Router View for different pages -->
-  <router-view></router-view>
+  <div id="app">
+    <!-- Router View for different pages -->
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'App',
   components: {
-    
-  },
-};
+  }
+}
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #000000;
-  margin-top: 60px;
+
+  /* Achtergrondafbeelding */
+  background-image: url(~@/assets/background.jpg); /* Zorg dat dit pad klopt */
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+
+  width: 100%;
+  min-height: 100vh;
 }
 
+/* Navigation styling */
 nav {
   margin: 20px 0;
 }
@@ -37,5 +45,13 @@ nav a {
 
 nav a:hover {
   text-decoration: underline;
+}
+</style>
+
+<!-- Deze style is niet scoped en reset globale marges van html en body -->
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
 }
 </style>
